9d89d0c59209393a64eb97e639498949e332964e
Thiago Bruno <tbgt35@hotmail.com>
2023-06-03 12:22:43 -0300

    Iniciando estilização no Headers

---
+++ src/components/Headers/Dropdowns.jsx
@@ -0,0 +1,124 @@
+import Link from 'next/link';
+
+export const Dorpdowns = () => {
+  return (
+    <div className="relative inline-block text-left">
+      <div>
+        <button
+          type="button"
+          className="inline-flex w-40 justify-evenly gap-x-1.5 rounded-full bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 border border-white"
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+        >
+          Produtos
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25"
+            />
+          </svg>
+        </button>
+      </div>
+
+      {/* <!--
+    Dropdown menu, show/hide based on menu state.
+
+    Entering: "transition ease-out duration-100"
+      From: "transform opacity-0 scale-95"
+      To: "transform opacity-100 scale-100"
+    Leaving: "transition ease-in duration-75"
+      From: "transform opacity-100 scale-100"
+      To: "transform opacity-0 scale-95"
+  --> */}
+      <div
+        className="absolute  right-0 left-0 z-10 mt-2 w-50 origin-top-right rounded-md bg-sky-50 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="menu-button"
+        tabIndex="-1"
+      >
+        <div className="py-1" role="none">
+          {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
+          <Link
+            href="#"
+            className="text-gray-700 px-4 py-2 text-sm flex"
+            role="menuitem"
+            tabIndex="-1"
+            id="menu-item-0"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 mr-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+              />
+            </svg>
+            Brindes
+          </Link>
+          <Link
+            href="#"
+            className="text-gray-700 flex px-4 py-2 text-sm"
+            role="menuitem"
+            tabIndex="-1"
+            id="menu-item-1"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 mr-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42"
+              />
+            </svg>
+            Personalizados
+          </Link>
+          <Link
+            href="#"
+            className="text-gray-700 flex px-4 py-2 text-sm"
+            role="menuitem"
+            tabIndex="-1"
+            id="menu-item-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 mr-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"
+              />
+            </svg>
+            Papelaria
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Headers/Headers.jsx
@@ -0,0 +1,58 @@
+import Image from 'next/image';
+import { Search } from './Search';
+import { Dorpdowns } from './Dropdowns';
+import Link from 'next/link';
+
+export const Headers = () => {
+  return (
+    <header className="flex flex-col w-full">
+      <section className="flex justify-between bg-sky-100 text-purple-950 items-center">
+        <div>
+          <p>numero</p>
+        </div>
+        <div>
+          <p>promoção </p>
+        </div>
+        <div>
+          <p>rastreio de pedido</p>
+        </div>
+      </section>
+      <section className="flex justify-around bg-indigo-700 text-white py-1.5 items-center">
+        <div className="flex justify-between w-1/2 border border-white items-center">
+          <Image
+            src="/issets/imageHeaders/logoWhiteSemfundo.png"
+            width={100}
+            height={100}
+            alt="Logo VPapel"
+          />
+          <Search />
+        </div>
+
+        <div>
+          <nav>
+            <Dorpdowns />
+          </nav>
+        </div>
+        <div className="flex items-center justify-between  w-20">
+          <Image src="/issets/imageHeaders/instagram.png" width={35} height={35} />
+          <Link href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+          </Link>
+        </div>
+      </section>
+    </header>
+  );
+};
